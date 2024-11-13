e9e706dadc3a409f4ad01258e5bd2b8a9c8caa7f
krittin-ch <130139478+krittin-ch@users.noreply.github.com>
2024-11-13 13:33:15 +0700

    Update

--- bbox_human.py
+++ bbox_human.py
@@ -6,8 +6,8 @@ import cv2
 import numpy as np
 
 # logging.basicConfig(level=logging.CRITICAL)
-sys.stdout = open(os.devnull, 'w')
-sys.stderr = open(os.devnull, 'w')
+# sys.stdout = open(os.devnull, 'w')
+# sys.stderr = open(os.devnull, 'w')
 
 model = YOLO("yolo11n.pt", verbose=False)
 
@@ -25,10 +25,15 @@ def gen_bbox_human(img, acc=0.5):
     acc_res = acc_res[mark_acc]
 
     pos_res = bbox.xyxy.cpu().detach().numpy()[mark_people][mark_acc]
-    
+
+    # img = draw_rect(img, pos_res)
+
+    return pos_res
+
+def draw_rect(img, pos_res):
     color = (255, 0, 255)
     thickness = 4
-
+    
     for pos in pos_res:
         x1, y1, x2, y2 = pos
 
@@ -37,9 +42,8 @@ def gen_bbox_human(img, acc=0.5):
 
 
         img = cv2.rectangle(img, start_point, end_point, color, thickness)
-
+    
     return img
-
 # img = cv2.imread("bus.jpg")
 
 # res = gen_bbox_human(img)
